2318fb55311c6087cb1405482ba27da3e1fb4a05
sharif.karimi <sharif.karimi@mediendesign.de>
2025-05-01 19:19:12 +0200

    add axios and connect to laravel

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -2,22 +2,36 @@
 import { ref } from "vue";
 import GuestLayout from "../components/GuestLayout.vue";
 import { RouterLink } from "vue-router";
+import axiosClient from "../axios.js";
 
+const errors = ref({}); // ✅ define errors here
 const data = ref({
     name: "",
     email: "",
     password: "",
-    passwordConfirmation: "",
+    password_confirmation: "",
 });
-</script>
 
+function submit() {
+  axiosClient.get('/sanctum/csrf-cookie').then(response => {
+    axiosClient.post("/register", data.value)
+        .then(response => {
+          router.push({name: 'Home'})
+        })
+        .catch(error => {
+          console.log(error.response.data)
+          errors.value = error.response.data.errors;
+        })
+  });
+}
+</script>
 
 <template>
     <GuestLayout>
         <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create new account</h2>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6">
+            <form @submit.prevent="submit" class="space-y-4">
                 <div>
                     <label for="name" class="block text-sm/6 font-medium text-gray-900">Full Name</label>
                     <div class="mt-2">
@@ -44,7 +58,7 @@ const data = ref({
                         <label for="passwordConfirmation" class="block text-sm/6 font-medium text-gray-900">Password Confirmation</label>
                     </div>
                     <div class="mt-2">
-                        <input type="password" name="passwordConfirmation" id="passwordConfirmation"  required v-model="data.passwordConfirmation" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input type="password" name="passwordConfirmation" id="passwordConfirmation" required v-model="data.password_confirmation" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
 
